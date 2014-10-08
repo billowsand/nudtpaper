b805628a2fa5eec0aa614c48498a9f847cddf728
Marmot <billowsand@icloud.com>
2014-10-08 20:13:00 +0800

    Update makepdf.bat

--- makepdf.bat
+++ makepdf.bat
@@ -1,7 +1,7 @@
 del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi data\*.aux
-xelatex thesis
+xelatex -shell-escape thesis
 rem makeindex -s nomencl.ist -o thesis.nls thesis.nlo
 bibtex thesis
 bibtex thesis
-xelatex thesis
-xelatex thesis
+xelatex -shell-escape thesis
+xelatex -shell-escape thesis
